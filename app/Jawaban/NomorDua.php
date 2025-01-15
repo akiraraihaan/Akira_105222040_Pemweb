20cef021097c98ab0e13b4cf32ba0cc013c4335c
Akira <raihanakirar@gmail.com>
2025-01-15 17:51:13 +0700

    nomor 2 done

--- app/Jawaban/NomorDua.php
+++ app/Jawaban/NomorDua.php
@@ -11,8 +11,18 @@ class NomorDua {
 	public function submit (Request $request) {
 
 		// Tuliskan code untuk menyimpan data Jadwal
-		
-		return redirect()->route('event.home');
+		$event = new Event();
+		$event->user_id = Auth::id();
+		$event->name = $request->nama;
+		$event->start = $request->start;
+		$event->end = $request->end;
+		$event->save();
+
+		if($event->save()){
+			return redirect()->route('event.home')->with('message', ['Jadwal berhasil ditambahkan', 'success']);
+		}else{
+			return redirect()->route('event.home')->with('message', ['Jadwal gagal ditambahkan', 'danger']);
+		}
 	}
 }
 
--- resources/views/jawaban/NomorDua/index.blade.php
+++ resources/views/jawaban/NomorDua/index.blade.php
@@ -1,6 +1,7 @@
 <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel" aria-hidden="true">
  	<div class="modal-dialog modal-dialog-centered" role="document">
     	<form class="modal-content" method="POST" action="{{ route('event.submit') }}">
+            @csrf
       		<div class="modal-header">
         		<h5 class="modal-title" id="addModalLabel">Tambah Jadwal</h5>
         		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
